273666404273de5f107faa6696fec82b400d1003
Rohithjem <128702344+Rohithjem@users.noreply.github.com>
2024-09-11 08:29:09 +0530

    Create 4. Write a program to get the largest element of an array.c

---
+++ 4. Write a program to get the largest element of an array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter size: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("Largest number is : %d",max);
+}
